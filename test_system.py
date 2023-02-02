397d5ff7caafc7a65fc657d1abf0162f51e054ce
Alex Nordstrom <6267244+HyperEpsilon@users.noreply.github.com>
2023-02-01 18:28:43 -0700

    ci: add python ci system test

---
+++ test_system.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+def test_code():
+    r = requests.get()
+    json_response = r.json()
+    assert(json_response["code"] == 404)
+
+def test_message():
+    r = requests.get()
+        json_response = r.json()
+        assert(json_response["message"] == "")
+
